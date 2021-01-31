34c24641c1839ad90ab3ac854134808c8164fea5
alexandre <alexandre.vanhoutte@gmail.com>
2021-01-31 13:37:21 +0900

    feat(api_basic_auth): user can get token

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult} = require("express-validator");
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 const User = require('../model/User')
+
+require('dotenv').config()
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -60,6 +63,61 @@ router.post(
     } catch(err) {
       console.log(err.message);
       res.status(500).send("Error in saving")
+    }
+  }
+)
+
+router.post(
+  "/login",
+  [
+    check("email", "Please enter a valid email").isEmail(),
+    check("password", "Please enter a valid password").isLength({
+      min: 6
+    })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array()
+      })
+    }
+
+    const {email, password} = req.body;
+    try {
+      let user = await User.findOne({
+        email
+      })
+      if (!user) {
+        return res.status(400).json({msg: "User not exist"})
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch)
+        return res.status(400).json({
+          message: "Incorrect Password !"
+        });
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        "randomString",
+        {
+          expiresIn: 3600
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.status(200).json({token});
+        }
+      );
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({message: "Server Error"});
     }
   }
 )
