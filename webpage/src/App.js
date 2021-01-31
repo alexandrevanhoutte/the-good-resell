8f650bcc2ce0dd6713ca1972726a20f92a03892f
alexandre <alexandre.vanhoutte@gmail.com>
2021-01-31 17:31:29 +0900

    feat(webpage authentication): user can login

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -6,11 +6,12 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
 import Preferences from './components/Preferences/Preferences';
 import Login from './components/Login/Login';
-
+import useToken from './useToken';
 
 function App() {
-  const [token, setToken] = useState();
+  const {token, setToken } = useToken();
 
+  console.log(token)
   if(!token) {
     return <Login setToken={setToken} />
   }
